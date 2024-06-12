92bf81cfd2143a48760530d55391f55a8060407d
narsimha-na <activena1@gmail.com>
2024-06-12 19:42:41 +0530

    hij

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -13,7 +13,7 @@ export default function Footer() {
         <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
           Theme by{" "}
           <a href="https://github.com/saadpasta/developerFolio">
-            developerFolio
+            developer Folio
           </a>
         </p>
       </div>
